8b8bb66885c5e31b3fa95d7a4adee435441c8ecf
Alexander Botteram <a.botteram@gmail.com>
2018-10-09 08:49:07 +0200

    Removed superfluous slashes

--- dev-blogger.php
+++ dev-blogger.php
@@ -23,10 +23,10 @@ $manager->register_integration( new Devb_Capabilities_Manager() );
 $manager->run();
 
 function devb_enqueue_block_editor_assets() {
-	wp_register_script( 'devb_babel', plugins_url( '/node_modules/@babel/standalone/babel.min.js', __FILE__ ) );
-	wp_register_script( 'devb_babel_preset_env', plugins_url( '/node_modules/@babel/preset-env-standalone/babel-preset-env.min.js', __FILE__ ), array( 'devb_babel' ) );
+	wp_register_script( 'devb_babel', plugins_url( 'node_modules/@babel/standalone/babel.min.js', __FILE__ ) );
+	wp_register_script( 'devb_babel_preset_env', plugins_url( 'node_modules/@babel/preset-env-standalone/babel-preset-env.min.js', __FILE__ ), array( 'devb_babel' ) );
 	wp_add_inline_script( 'devb_babel_preset_env', 'if ( ! window.devb ) window.devb = {}; window.devb.Babel = window.Babel; window.Babel = undefined;', 'after' );
-	wp_register_script( 'devb_edit_page', plugins_url( '/js/dist/edit-page.js', __FILE__ ), array( 'devb_babel_preset_env' ), null, true );
+	wp_register_script( 'devb_edit_page', plugins_url( 'js/dist/edit-page.js', __FILE__ ), array( 'devb_babel_preset_env' ), null, true );
 
 	wp_enqueue_script( 'devb_edit_page' );
 }
